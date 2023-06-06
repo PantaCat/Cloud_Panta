8600db352b47123c1f03685a7a102445fb23541e
“PantaCat” <173774890@qq.com>
2023-06-06 14:17:43 +0800

    新增servicedemo项目并实现通过网关访问

--- ServiceDemo/src/main/java/panta/ServiceDemoApplication.java
+++ ServiceDemo/src/main/java/panta/ServiceDemoApplication.java
@@ -1,18 +1,15 @@
-package org.panta;
+package panta;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 
-@EnableZuulProxy
 @EnableEurekaClient
 @SpringBootApplication
-public class ZuulApplication {
+public class ServiceDemoApplication {
     public static void main(String []args){
-        //Zuul启动类
-        SpringApplication.run(ZuulApplication.class);
+        //启动类
+        SpringApplication.run(ServiceDemoApplication.class);
     }
 
 }
