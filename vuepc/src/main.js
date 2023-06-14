66129bb7dbaa24b80ab8deb792ab29ed30c9729f
“PantaCat” <173774890@qq.com>
2023-06-14 18:04:20 +0800

    VUE

--- vuepc/src/main.js
+++ vuepc/src/main.js
@@ -1,5 +1,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+import VueQuillTextEditor from 'vue-quill-text-editor'
+import 'vue-quill-text-editor/dist/vue-quill-text-editor.esm.css'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+app.use(ElementPlus, {
+  size: 'small',
+  zIndex: 3000,
+  locale: zhCn
+})
+app.use(router)
+app.use(VueQuillTextEditor)
+app.mount('#app')
---
+++ vuepc/src/page/MenuLeft.vue
@@ -0,0 +1,38 @@
+<template>
+  <el-row class="tac">
+    <el-col :span="4">
+      <h5 class="mb-2" align="center">导航栏</h5>
+      <el-menu
+        router
+        :default-active="this.$route.path"
+        class="el-menu-vertical-demo"
+        style="height: calc(100% - 60px);overflow: auto;"
+      >
+        <el-menu-item index="/" align="center"><img style="height: 30px;width: 30px;" src="../assets/logo.png">&nbsp;&nbsp;首页</el-menu-item>
+        <el-sub-menu index="1">
+          <template #title>
+            <el-icon><location /></el-icon>
+            <span>记录本</span>
+          </template>
+            <el-menu-item index="/about">临时区域</el-menu-item>
+            <el-menu-item index="1-2">item two</el-menu-item>
+          <el-sub-menu index="1-4">
+            <template #title>item four</template>
+            <el-menu-item index="1-4-1">item one</el-menu-item>
+          </el-sub-menu>
+        </el-sub-menu>
+      </el-menu>
+    </el-col>
+    <router-view/>
+  </el-row>
+</template>
+
+<script>
+export default {
+  name: 'MenuLeft'
+}
+</script>
+
+<style scoped>
+
+</style>
