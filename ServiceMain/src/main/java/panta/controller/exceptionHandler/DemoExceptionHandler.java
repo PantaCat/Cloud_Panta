83c59dcdbb137937a98cac61df9a959dcc226e96
“PantaCat” <173774890@qq.com>
2023-06-27 15:28:08 +0800

    /

---
+++ ServiceMain/src/main/java/panta/controller/exceptionHandler/DemoExceptionHandler.java
@@ -0,0 +1,31 @@
+package panta.controller.exceptionHandler;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import panta.model.demo.DemoModel;
+import panta.service.demo.DemoService;
+
+import java.math.BigDecimal;
+
+@RestControllerAdvice
+public class DemoExceptionHandler {
+    @Autowired
+    private DemoService demoService;
+
+    /*
+     *@ExceptionHandler注解中可以添加参数，
+     *参数是某个异常类的class，代表这个方法专门处理该类异常，比如这样：
+     */
+    @ExceptionHandler(ArithmeticException.class)
+    public String arithmeticExceptionHandler(Exception exception){
+        exception.printStackTrace();
+        String message = "[DemoExceptionHandler]-此类中发生了控制正异常，请在此处理";
+        System.out.println(message);
+        DemoModel d = new DemoModel();
+        d.setName(message);
+        d.setNumber(new BigDecimal("100"));
+        demoService.addDemoLog(d);
+        return message;
+    }
+}
